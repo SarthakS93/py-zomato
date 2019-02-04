927e22d07be113d1c0c82b5cbeacf60ab240544b
sarthak <sarthaks93@gmail.com>
2019-02-04 23:26:09 +0530

    added get location info method

--- zomato.py
+++ zomato.py
@@ -14,6 +14,17 @@ def getCategories():
     print(r.json())
     return r.json()
 
+def getLocationInfo(lat = '12.927923', lng = '77.627106'):
+    headers = {'user-key': config.API_KEY}
+    params = {'lat': lat, 'lon': lng}
+    r = requests.get(API_URL + '/geocode', params = params, headers = headers)
+    #print(r.status_code)
+    #print(r)
+    print(r.json())
+    print(r.url)
+    return r.json()
 
-getCategories()
+
+#getCategories()
+getLocationInfo()
 
